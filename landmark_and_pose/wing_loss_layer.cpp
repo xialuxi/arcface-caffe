7cbe27c063dd6fb21fb8e02c761944c43797d0a4
xialuxi <297509086@qq.com>
2019-01-14 20:15:58 +0800

    Add files via upload

---
+++ landmark_and_pose/wing_loss_layer.cpp
@@ -0,0 +1,128 @@
+#include <vector>
+
+#include "caffe/filler.hpp"
+#include "caffe/layers/wing_loss_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+
+namespace caffe {
+
+template <typename Dtype>
+void WingLossLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
+{
+    w = this->layer_param_.wing_loss_param().w();
+    DCHECK(w > 0);
+    epsilon = this->layer_param_.wing_loss_param().epsilon();
+    DCHECK(epsilon >= 0);
+
+
+    //_c = w * (1.0 - log(1.0 + w/epsilon)
+    _c = w * (1.0 - log(1.0 + w/epsilon));
+
+   // has_ignore_label_ = this->layer_param_.loss_param().has_ignore_label();
+   // if (has_ignore_label_)
+   // {
+   //     ignore_label_ = this->layer_param_.loss_param().ignore_label();
+   // }
+}
+
+template <typename Dtype>
+void WingLossLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
+{
+    LossLayer<Dtype>::Reshape(bottom, top);
+
+    //x = landmarks - labels
+    diff_.ReshapeLike(*bottom[0]);
+    abs_x.ReshapeLike(*bottom[0]);
+    log_abs.ReshapeLike(*bottom[0]);
+}
+
+template <typename Dtype>
+void WingLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top)
+{
+   
+    const Dtype* bottom_data = bottom[0]->cpu_data();
+    const Dtype* label_data = bottom[1]->cpu_data();
+    
+    Dtype* sub_x_data = diff_.mutable_cpu_data();
+    Dtype* abs_x_data = abs_x.mutable_cpu_data();
+    Dtype* log_abs_data = log_abs.mutable_cpu_data();
+
+    int count = bottom[0]->count();
+
+    caffe_set(count, Dtype(0), sub_x_data);
+    caffe_set(count, Dtype(0), abs_x_data);
+    caffe_set(count, Dtype(1.0), log_abs_data);
+
+    caffe_sub(count, bottom_data, label_data, sub_x_data);
+    caffe_abs(count, sub_x_data, abs_x_data);
+    const Dtype scale = Dtype(1.0 / epsilon);
+    caffe_axpy(count, scale, abs_x_data, log_abs_data);
+    caffe_log(count, log_abs_data, log_abs_data);
+    caffe_scal(count, w, log_abs_data);
+    
+    Dtype loss = 0.f;
+    for(int i = 0; i < count; i++)
+    {
+        if(w > abs_x_data[i])
+        {
+            loss += log_abs_data[i];
+        }
+        else
+        {
+            loss += abs_x_data[i] - _c;
+        }
+    }
+    
+    loss = loss / bottom[0]->num();
+    top[0]->mutable_cpu_data()[0] = loss;
+}
+
+template <typename Dtype>
+void WingLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+                                       const vector<bool>& propagate_down,
+                                       const vector<Blob<Dtype>*>& bottom)
+{
+    const Dtype* sub_x_data = diff_.cpu_data();
+    const Dtype* abs_x_data = abs_x.cpu_data();
+    Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+  
+    int count = bottom[0]->count();
+
+    if (propagate_down[0])
+    {
+        for(int i = 0; i < count; i++)
+        {
+            if(w > abs_x_data[i])
+            {
+                if(sub_x_data[i] > 0) {
+                    bottom_diff[i] = w / (sub_x_data[i] + epsilon); 
+                  }
+                else{
+                    bottom_diff[i] = w / (sub_x_data[i] - epsilon);                
+                  }
+            }
+            else
+            {
+                if(sub_x_data[i] > 0) {
+                    bottom_diff[i] = 1; 
+                  }
+                else{
+                    bottom_diff[i] = -1;                
+                  }
+            }
+        }
+    }
+
+    Dtype loss_weight = top[0]->cpu_diff()[0];
+    caffe_scal(count, loss_weight / bottom[0]->num(), bottom_diff);
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(WingLossLayer);
+#endif
+
+INSTANTIATE_CLASS(WingLossLayer);
+REGISTER_LAYER_CLASS(WingLoss);
+
+}  // namespace caffe
---
+++ landmark_and_pose/wing_loss_layer.hpp
@@ -0,0 +1,53 @@
+#ifndef CAFFE_WING_LOSS_LAYER_HPP_
+#define CAFFE_WING_LOSS_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+#include "caffe/layers/loss_layer.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+class WingLossLayer : public LossLayer<Dtype> {
+ public:
+  explicit WingLossLayer(const LayerParameter& param)
+      : LossLayer<Dtype>(param), diff_() {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "WingLoss"; }
+  virtual inline bool AllowForceBackward(const int bottom_index) const {
+    return true;
+  }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //    const vector<Blob<Dtype>*>& top);
+
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  Dtype w;
+  Dtype epsilon;
+  Dtype _c;
+  
+  bool has_ignore_label_;
+  
+  Blob<Dtype> diff_;
+  Blob<Dtype> abs_x;
+  Blob<Dtype> log_abs;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_WING_LOSS_LAYER_HPP_
