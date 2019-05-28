d47068e8cccbbb0b49c427f0af6e5a941da5cf7e
xialuxi <297509086@qq.com>
2019-05-28 18:41:09 +0800

    Update adacos_add_m_scale_layer.hpp

--- AdaCos/adacos_add_m_scale_layer.hpp
+++ AdaCos/adacos_add_m_scale_layer.hpp
@@ -29,13 +29,13 @@ class AdaCosAddmScaleLayer : public Layer<Dtype> {
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-  //    const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
 
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
   Dtype m_;
   Dtype sin_m;
